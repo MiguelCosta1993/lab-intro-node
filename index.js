9c1854fe4b449672e89c279b88fe75fce2a571da
Miguel Costa <miguelcosta1993@gmail.com>
2020-07-20 15:21:46 +0100

    all iterations + bonus

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    if (this.items.length) {
+      this.items.sort((a, b) => {
+        return a - b;
+      });
+      this.length = this.items.length;
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length) {
+      return this.items.reduce((a, b) => a + b, 0);
+    } else if (this.length === 0) return 0;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length) {
+      return this.items.reduce((a, b) => a + b, 0) / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
